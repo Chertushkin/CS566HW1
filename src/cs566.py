71490bf8793c2d8b3cd716200bd28f42df383106
Chertushkin <chertushkinestate@gmail.com>
2023-09-03 00:11:16 -0400

    Add template

--- src/cs566.py
+++ src/cs566.py
@@ -17,12 +17,7 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for n in nums:
-            if n in seen:
-                return True
-            seen.add(n)
-        return False
+        # TODO: implement your solution 
 
 """### Do not modify the testing code below. If you get message "Mistake in test case #", it means that you algorithm is incorrect."""
 
